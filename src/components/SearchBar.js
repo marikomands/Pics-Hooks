9375b4b9bf9fdedf3b16fdbb8b3d37a0522c6ba7
marikomands <marikomands@gmail.com>
2022-07-13 11:20:52 +0100

    controlled elements and invoking callbacks

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchBar.css";
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+  const [term, setTerm] = useState("");
+  console.log(term);
+
+  const onFormSubmit = (event) => {
+    event.defaultPrevented();
+    props.onSubmit(term);
+  };
+
   return (
     <div className="container">
-      <form className="form">
+      <form className="form" onSubmit={onFormSubmit}>
         <label className="font">Image Search</label>
-        <input className="input" type="text" />
+        <input
+          className="input"
+          value={term}
+          type="text"
+          onChange={(e) => setTerm(e.target.value)}
+        />
       </form>
     </div>
   );
